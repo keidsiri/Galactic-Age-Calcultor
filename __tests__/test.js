8518ef858f2e013c9a7f878b3685eea8f42da458
Kate Kiatsiri <keidsiri@gmail.com>
2021-11-11 09:53:20 -0800

    Good failed on the first test , constructor is empty , received undefined

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,9 +1,9 @@
-import {UserInfo} from './../src/business';
+import Calculator from './../src/business.js'
 
-describe('UserInfo', () => {
-  test ('Should create new user object with name and age', () => {
-    const user = new UserInfo('Kate', '30');
+describe ('Calculator', () => {
+  test('Should create new user object with name and age', () => {
+    const user = new Calculator ('Kate', 30);
     expect(user.name).toEqual('Kate');
     expect(user.age).toEqual(30);
-  });
-});
+  })
+})
