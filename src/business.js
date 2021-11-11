e394ae966daf0e12c00bd4412848bf082dad4727
Kate Kiatsiri <keidsiri@gmail.com>
2021-11-11 12:07:57 -0800

    Test5 :  passed , expected value is matched

--- src/business.js
+++ src/business.js
@@ -5,7 +5,7 @@ export default class Calculator{
     this.mercuryAge = (this.age / 0.24);
     this.venusAge = (this.age / 0.62);
     this.marsAge = (this.age / 1.88);
-    this.jupiterAge = 0;
+    this.jupiterAge = (this.age / 11.88);
   }
 }
 
